ff13fcc16287921c29d83104a5949d64439e6c37
Philippe VILLET <pvillet@hotmail.com>
2020-04-29 14:20:29 +0200

    Remove debug traces

--- disp.sh
+++ disp.sh
@@ -5,26 +5,26 @@ while true
 do
    feh -Y -F -Z -D 1 /home/pi/Diffusion&
    feh_pid=$!
-   echo "feh PID"
-   echo $feh_pid
+#   echo "feh PID"
+#   echo $feh_pid
    change=0
    while [ $change -eq 0 ] 
    do
-      echo "Checking"
+#      echo "Checking"
       rclone check remote:01_Commission_Communication/Diffusion_Photos /home/pi/Diffusion
       res=$?
-      echo $res
+#      echo $res
       if [ $res -gt 0 ]
       then
-         echo "KILL"
-         echo $feh_pid
+         #echo "KILL"
+         #echo $feh_pid
          kill $feh_pid
          rclone sync remote:01_Commission_Communication/Diffusion_Photos /home/pi/Diffusion
          change=1
          sync
       else
-         echo "Sleep"
-         sleep 30 
+#         echo "Sleep"
+         sleep 60
       fi
    done
 
